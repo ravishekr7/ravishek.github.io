d2e32ac06b45c807017da1ad9d830a29a1ce0c84
Muhammad Essa <imuhammadessa@gmail.com>
2024-08-26 02:03:48 +0500

    Updated: contact me page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 function SocialLink({ icon: Icon, ...props }) {
   return (
     <Link className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-blue-600 dark:fill-blue-400 dark:group-hover:fill-blue-300" />
     </Link>
   )
 }
