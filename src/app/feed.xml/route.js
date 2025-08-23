6819263e1e3b269dba3d6fdf722fe9191144a25b
Ravishek Ranjan <ravishek@Ravisheks-MacBook-Air.local>
2025-08-23 21:59:09 +0530

    removed dynamic

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -1,8 +1,9 @@
-import assert from 'assert'
-import * as cheerio from 'cheerio'
 import { Feed } from 'feed'
+import { getAllArticles } from '@/lib/articles'
 
-export async function GET(req) {
+export const dynamic = 'force-static'
+
+export async function GET() {
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   if (!siteUrl) {
@@ -16,7 +17,7 @@ export async function GET(req) {
 
   let feed = new Feed({
     title: author.name,
-    description: 'Your blog description',
+    description: 'Data Engineer passionate about building scalable data pipelines',
     author,
     id: siteUrl,
     link: siteUrl,
@@ -28,35 +29,20 @@ export async function GET(req) {
     },
   })
 
-  let articleIds = require
-    .context('../articles', true, /\/page\.mdx$/)
-    .keys()
-    .filter((key) => key.startsWith('./'))
-    .map((key) => key.slice(2).replace(/\/page\.mdx$/, ''))
+  let articles = await getAllArticles()
 
-  for (let id of articleIds) {
-    let url = String(new URL(`/articles/${id}`, req.url))
-    let html = await (await fetch(url)).text()
-    let $ = cheerio.load(html)
-
-    let publicUrl = `${siteUrl}/articles/${id}`
-    let article = $('article').first()
-    let title = article.find('h1').first().text()
-    let date = article.find('time').first().attr('datetime')
-    let content = article.find('[data-mdx-content]').first().html()
-
-    assert(typeof title === 'string')
-    assert(typeof date === 'string')
-    assert(typeof content === 'string')
+  for (let article of articles) {
+    let publicUrl = `${siteUrl}/articles/${article.slug}/`
 
     feed.addItem({
-      title,
+      title: article.title,
       id: publicUrl,
       link: publicUrl,
-      content,
+      description: article.description,
+      content: article.description, // Using description as content since we can't fetch HTML statically
       author: [author],
       contributor: [author],
-      date: new Date(date),
+      date: new Date(article.date),
     })
   }
 
