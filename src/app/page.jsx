bee49979b8dc91211c4839a2783b8b2f3c4f819e
Ravishek Ranjan <ravishek@Ravisheks-MacBook-Air.local>
2025-08-23 21:16:06 +0530

    Removed certificate section temp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -204,19 +204,19 @@ function Certificates() {
     },
   ]
 
-  return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <CertificationsIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Certifications</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {certificates.map((certificate, index) => (
-          <Certificate key={index} certificate={certificate} />
-        ))}
-      </ol>
-    </div>
-  )
+  // return (
+  //   <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+  //     <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+  //       <CertificationsIcon className="h-6 w-6 flex-none" />
+  //       <span className="ml-3">Certifications</span>
+  //     </h2>
+  //     <ol className="mt-6 space-y-4">
+  //       {certificates.map((certificate, index) => (
+  //         <Certificate key={index} certificate={certificate} />
+  //       ))}
+  //     </ol>
+  //   </div>
+  // )
 }
 
 function Resume() {
